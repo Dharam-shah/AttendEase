ab798b79d61017112baa2f63dd4d6eec184abf0f
Gaurav Jaiswal <gaurav.jaiswal@khalti.com>
2023-10-23 17:13:32 +0545

    fix issues

--- apps/lecturer/forms.py
+++ apps/lecturer/forms.py
@@ -4,11 +4,11 @@ from .models import Lecture
 class LectureForm(forms.ModelForm):
     title = forms.CharField(label='Lecture title:', max_length = 101),
     start_date = forms.DateTimeField(label='Start date:', widget=forms.DateInput(attrs={'type': 'date'}))
-    start_time = forms.DateTimeField(label='Start time:', widget=forms.DateInput(attrs={'type': 'time'}))
+    start_time = forms.TimeField(label='Start time:', widget=forms.DateInput(attrs={'type': 'time'}))
     end_date = forms.DateTimeField(label='End date:', widget=forms.DateInput(attrs={'type': 'date'}))
-    end_time = forms.DateTimeField(label='End time:', widget=forms.DateInput(attrs={'type': 'time'}))
+    end_time = forms.TimeField(label='End time:', widget=forms.DateInput(attrs={'type': 'time'}))
 
     class Meta:
         model = Lecture
-        fields = ['title', 'start_time', 'start_time', 'end_time', 'course', 'classroom']
+        fields = ['title', 'start_date', 'start_time', 'end_time', 'end_time', 'course', 'classroom']
 
--- apps/lecturer/views.py
+++ apps/lecturer/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from django.views.generic.edit import CreateView
 from django.views.generic import ListView, DetailView 
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+
+from apps.users.models import Teacher
 from .models import Lecture
 from .forms import LectureForm
 # Create your views here.
@@ -14,6 +18,12 @@ class CreateLectureView(CreateView):
     form_class = LectureForm
     template_name = 'dashboard/lecture/create_lecture.html'
     success_url = reverse_lazy('lecture-list')
+
+    def form_valid(self, form):
+        if not self.request.user.is_teacher:
+            return HttpResponse(status=400, data="Not Allowed")
+        form.instance.lecturer = self.request.user.teacher.id
+        return super().form_valid(form)
 
 
 class LectureListView(ListView):
